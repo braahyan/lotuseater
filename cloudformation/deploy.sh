990bb4af5ed6721edbbdd81802da924f41ff3750
Bryan Pedlar <bryan@thrivehive.com>
2018-12-18 17:30:55 -0500

    testing build pipeline

--- cloudformation/deploy.sh
+++ cloudformation/deploy.sh
@@ -3,9 +3,9 @@
 NETWORK="00-network"
 security_groups="01-securityGroups"
 aurora="02-aurora"
-s3processor="03-SAMs3processor"
+s3processor="03-aws cloudformations3processor"
 kinesis="04-kinesis"
-kinesisCollector="05-SAMkinesisCollector"
+kinesisCollector="05-aws cloudformationkinesisCollector"
 continuousIntegration="06-continuousIntegration"
 bucketName="bryanpedlar-dev"
 
@@ -14,31 +14,31 @@ if [[ $1 = "package" ]]; then
     pip install -r ../src/requirements.txt -t ../src/modules
 fi
 
-sam package --template-file "$NETWORK.yml" \
+aws cloudformation package --template-file "$NETWORK.yml" \
             --s3-bucket $bucketName \
             --output-template-file $NETWORK.packaged.yml
 
-sam package --template-file "$security_groups.yml" \
+aws cloudformation package --template-file "$security_groups.yml" \
             --s3-bucket $bucketName \
             --output-template-file $security_groups.packaged.yml
 
-sam package --template-file "$aurora.yml" \
+aws cloudformation package --template-file "$aurora.yml" \
             --s3-bucket $bucketName \
             --output-template-file $aurora.packaged.yml
 
-sam package --template-file "$s3processor.yml" \
+aws cloudformation package --template-file "$s3processor.yml" \
             --s3-bucket $bucketName \
             --output-template-file $s3processor.packaged.yml
 
-sam package --template-file "$kinesis.yml" \
+aws cloudformation package --template-file "$kinesis.yml" \
             --s3-bucket $bucketName \
             --output-template-file $kinesis.packaged.yml
 
-sam package --template-file "$kinesisCollector.yml" \
+aws cloudformation package --template-file "$kinesisCollector.yml" \
             --s3-bucket $bucketName \
             --output-template-file $kinesisCollector.packaged.yml
 
-sam package --template-file "$continuousIntegration.yml" \
+aws cloudformation package --template-file "$continuousIntegration.yml" \
             --s3-bucket $bucketName \
             --output-template-file $continuousIntegration.packaged.yml
 
@@ -50,7 +50,7 @@ aws s3api put-object --bucket $bucketName --body $kinesis.packaged.yml --key $ki
 aws s3api put-object --bucket $bucketName --body $kinesisCollector.packaged.yml --key $kinesisCollector.packaged.yml
 aws s3api put-object --bucket $bucketName --body $continuousIntegration.packaged.yml --key $continuousIntegration.packaged.yml
 
-sam deploy --template-file overstack.yml --parameter-overrides BucketName=$bucketName --stack-name overstack3 --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND
+#aws cloudformation deploy --template-file overstack.yml --parameter-overrides BucketName=$bucketName --stack-name overstack3 --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND
 
 rm -rf *packaged*.yml
 rm -rf /src/modules
